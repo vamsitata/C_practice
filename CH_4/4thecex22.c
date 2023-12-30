081de3bfaed3c24ff18e2899081416932700904c
vamsitata <120320073+vamsitata@users.noreply.github.com>
2023-12-30 09:49:59 +0530

    Delete CH_4 directory

--- CH_4/4thecex22.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int a,b,c;
-    printf("Enter the numbers:");
-    scanf("%d %d %d",&a,&b,&c);
-    (a>b && b>c) ? printf("greatest number is %d",a) :
-    (b>a && b>c) ? printf("greatest number is %d",b) :
-    printf("greatest number is %d",c);
-}
